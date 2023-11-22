ef64487757212fa654b97d753da2bcdb192b89a3
songluyu <songluyu@baidu.com>
2023-11-22 20:28:29 +0800

    fix bug

--- internal/rdb/rdb.go
+++ internal/rdb/rdb.go
@@ -1451,7 +1451,6 @@ func (r *RDB) CancelationPubSub() (*redis.PubSub, error) {
 	pubsub := r.client.Subscribe(ctx, base.CancelChannel)
 	_, err := pubsub.Receive(ctx)
 	if err != nil {
-		_ = pubsub.Close()
 		return nil, errors.E(op, errors.Unknown, fmt.Sprintf("redis pubsub receive error: %v", err))
 	}
 	return pubsub, nil
